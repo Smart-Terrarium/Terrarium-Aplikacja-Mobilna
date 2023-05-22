3a4e2729391ed9819ba7beeb73f7a57c40c4981a
s21161 <56189637+s21161@users.noreply.github.com>
2023-05-22 02:07:11 +0200

    Wiadomosc3

--- app/src/main/java/com/example/test02/ChartActivity.java
+++ app/src/main/java/com/example/test02/ChartActivity.java
@@ -1,10 +1,10 @@
 package com.example.test02;
-import org.json.*;
 
 import android.os.Bundle;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -12,8 +12,6 @@ import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import org.json.JSONException;
-import org.json.JSONObject;
 import java.net.URI;
 import java.net.URISyntaxException;
 import tech.gusavila92.websocketclient.WebSocketClient;
@@ -23,12 +21,12 @@ import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
 import android.graphics.Color;
-import android.os.Bundle;
-import android.util.Log;
-
-import androidx.appcompat.app.AppCompatActivity;
 import com.github.mikephil.charting.components.YAxis;
-
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.components.Legend;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 
@@ -37,7 +35,6 @@ public class ChartActivity extends AppCompatActivity {
     private LineChart lineChart;
     private LineDataSet lineDataSet;
     private LineData lineData;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,87 +48,205 @@ public class ChartActivity extends AppCompatActivity {
     private void createWebSocketClient() {
         URI uri;
         try {
-            uri = new URI("ws://192.168.88.252:8000/data");
+            uri = new URI("ws://192.168.88.251:8000/data");
         } catch (URISyntaxException e) {
             e.printStackTrace();
             return;
         }
+        class Sensor {
+            private final String id;
+            private final String timestamp;
+            private final double value;
 
+            public Sensor(String id, String timestamp, double value) {
+                this.id = id;
+                this.timestamp = timestamp;
+                this.value = value;
+            }
+
+            public String getId() {
+                return id;
+            }
+
+            public String getTimestamp() {
+                return timestamp;
+            }
+
+            public double getValue() {
+                return value;
+            }
+
+        }
+
+        class Device {
+            private final String id;
+            private final List<Sensor> sensors;
+
+            public Device(String id) {
+                this.id = id;
+                this.sensors = new ArrayList<>();
+            }
+
+            public String getId() {
+                return id;
+            }
+
+            public List<Sensor> getSensors() {
+                return sensors;
+            }
+        }
         webSocketClient = new WebSocketClient(uri) {
             @Override
             public void onOpen() {
-                Log.i("WebSocket", "Session is starting");
+                Log.i("WebSocket", "Rozpoczęto sesję");
                 webSocketClient.send("Hello World!");
-
             }
+
+            // Metoda pomocnicza do konwersji ciągu JSON na standardowy format
             public String convertStandardJSONString(String data_json) {
                 data_json = data_json.replaceAll("\\\\r\\\\n", "");
                 data_json = data_json.replace("\\\"", "\"");
-
                 data_json = data_json.replace("\"{", "{");
                 data_json = data_json.replace("}\",", "},");
                 data_json = data_json.replace("}\"", "}");
                 return data_json;
             }
 
-
-
             @Override
             public void onTextReceived(String s) {
-                Log.i("WebSocket", "Message received");
+                Log.i("WebSocket", "Odebrano wiadomość");
                 List<Device> devices = new ArrayList<>();
+                SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSSSS");
+                SimpleDateFormat outputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
                 try {
+                    // Parsowanie otrzymanego ciągu JSON
+
                     JSONObject json = new JSONObject(convertStandardJSONString(s));
                     Iterator<String> keys = json.keys();
                     while (keys.hasNext()) {
                         String id = keys.next();
                         JSONObject deviceObject = json.getJSONObject(id);
+
+                        // Tworzenie obiektu urządzenia
                         Device device = new Device(id);
+
+                        // Iteracja po czujnikach w danym urządzeniu
                         Iterator<String> sensorKeys = deviceObject.keys();
                         while (sensorKeys.hasNext()) {
                             String sensorId = sensorKeys.next();
                             JSONObject sensorObject = deviceObject.getJSONObject(sensorId);
+
+                            // Tworzenie obiektu czujnika i dodawanie go do listy czujników urządzenia
                             Sensor sensor = new Sensor(sensorId, sensorObject.getString("timestamp"), sensorObject.getDouble("value"));
                             device.getSensors().add(sensor);
                         }
+
+                        // Dodawanie urządzenia do listy urządzeń
                         devices.add(device);
                     }
+
+                    // Przetwarzanie danych czujników
                     for (Device device : devices) {
-                        System.out.println("Device ID: " + device.getId());
+                        System.out.println("ID urządzenia: " + device.getId());
                         List<Sensor> sensors = device.getSensors();
                         for (Sensor sensor : sensors) {
-                            System.out.println("Sensor ID: " + sensor.getId());
+                            System.out.println("ID czujnika: " + sensor.getId());
                             System.out.println("Timestamp: " + sensor.getTimestamp());
-                            System.out.println("Value: " + sensor.getValue());
+                            System.out.println("Wartość: " + sensor.getValue());
+                            String timestampString = sensor.getTimestamp();
+                            try {
+                                Date timestamp = inputFormat.parse(timestampString);
+                                String formattedTimestamp = outputFormat.format(timestamp);
+                                System.out.println("Przetworzony timestamp: " + formattedTimestamp);
+                            } catch (ParseException e) {
+                                e.printStackTrace();
+                            }
                             if (device.getId().equals("1") && sensor.getId().equals("8")) {
                                 runOnUiThread(new Runnable() {
                                     @Override
                                     public void run() {
                                         addEntryToChart(sensor.getValue());
                                     }
-                                });}}
+                                });
+                            }
+                        }
                     }
 
                 } catch (JSONException e) {
-                    Log.e("WebSocket", "Error parsing JSON", e);
+                    Log.e("WebSocket", "Błąd podczas parsowania JSON", e);
                 }
             }
+
+            // Metoda do dodawania wpisu do wykresu
             private void addEntryToChart(double value) {
                 if (lineDataSet == null) {
-                    lineDataSet = new LineDataSet(null, "Sensor 8");
+                    lineDataSet = new LineDataSet(null, "Czujnik 8");
+                    lineDataSet.setValueTextSize(20f);
                     lineDataSet.setAxisDependency(YAxis.AxisDependency.LEFT);
-                    lineDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
-                    lineDataSet.setCircleColors(ColorTemplate.MATERIAL_COLORS);
-                    lineDataSet.setLineWidth(2f);
+                    lineDataSet.setColor(Color.parseColor("#2196F3"));  // Ustawienie koloru linii
+                    lineDataSet.setCircleColor(Color.parseColor("#2196F3"));  // Ustawienie koloru punktów
+                    lineDataSet.setLineWidth(3f);
                     lineDataSet.setCircleRadius(4f);
+                    lineDataSet.setDrawFilled(true);  // Włączenie wypełnienia obszaru pod linią
                     lineDataSet.setFillAlpha(65);
-                    lineDataSet.setFillColor(ColorTemplate.MATERIAL_COLORS[0]);
+                    lineDataSet.setFillColor(Color.parseColor("#2196F3"));  // Ustawienie koloru wypełnienia
                     lineDataSet.setHighLightColor(Color.rgb(244, 117, 117));
-                    lineDataSet.setValueTextColor(Color.BLACK);
-                    lineDataSet.setValueTextSize(9f);
+                    lineDataSet.setValueTextColor(Color.BLUE);
+                    lineDataSet.setValueTextSize(20f);
+
+
+                    YAxis leftAxis = lineChart.getAxisLeft();
+                    leftAxis.setTextSize(15f);  // Zmiana rozmiaru czcionki dla etykiet osi Y
+
+                    YAxis rightAxis = lineChart.getAxisRight();
+                    rightAxis.setEnabled(false);  // Wyłączenie prawej osi
+
+                    XAxis xAxis = lineChart.getXAxis();
+                    xAxis.setTextSize(15f);  // Zmiana rozmiaru czcionki dla etykiet osi X
 
                     lineData = new LineData(lineDataSet);
                     lineChart.setData(lineData);
+
+                    lineChart.setDrawGridBackground(true);  // Włączenie tła siatki
+                    lineChart.setGridBackgroundColor(Color.LTGRAY);  // Ustawienie koloru tła siatki
+                    lineChart.getAxisLeft().setGridColor(Color.WHITE);  // Ustawienie koloru linii siatki dla osi Y
+                    lineChart.getXAxis().setGridColor(Color.WHITE);  // Ustawienie koloru linii siatki dla osi X
+
+                    lineChart.setBorderColor(Color.LTGRAY);  // Ustawienie koloru ramki
+
+                    Legend legend = lineChart.getLegend();
+                    legend.setEnabled(true);
+                    legend.setTextColor(Color.WHITE);
+                    legend.setTextSize(15f);
+
+                    lineChart.getDescription().setEnabled(false);  // Wyłączenie opisu wykresu
+                    lineChart.setDrawBorders(true);
+                    lineChart.setBorderColor(Color.BLACK);
+                    lineChart.setBorderWidth(5f);
+
+
+                    lineChart.setBackgroundColor(Color.DKGRAY);
+
+                    lineChart.getXAxis().setTextColor(Color.WHITE);
+                    lineChart.getAxisLeft().setTextColor(Color.WHITE);
+                    lineChart.getAxisRight().setTextColor(Color.WHITE);
+
+
+                    lineChart.getXAxis().setAxisLineWidth(2f);
+                    lineChart.getAxisLeft().setAxisLineWidth(2f);
+                    lineChart.getAxisRight().setAxisLineWidth(2f);
+
+                    lineChart.getXAxis().setGridLineWidth(2f);
+                    lineChart.getAxisLeft().setGridLineWidth(2f);
+                    lineChart.getAxisRight().setGridLineWidth(2f);
+
+
+
+
+
+
+
                 }
 
                 lineData.addEntry(new Entry(lineDataSet.getEntryCount(), (float) value), 0);
@@ -140,65 +255,34 @@ public class ChartActivity extends AppCompatActivity {
                 lineChart.setVisibleXRangeMaximum(10);
                 lineChart.moveViewToX(lineData.getEntryCount());
             }
-            class Sensor {
-                private String id;
-                private String timestamp;
-                private double value;
 
-                public Sensor(String id, String timestamp, double value) {
-                    this.id = id;
-                    this.timestamp = timestamp;
-                    this.value = value;
-                }
 
-                public String getId() {
-                    return id;
-                }
 
-                public String getTimestamp() {
-                    return timestamp;
-                }
 
-                public double getValue() {
-                    return value;
-                }
-            }
 
-            class Device {
-                private String id;
-                private List<Sensor> sensors;
-
-                public Device(String id) {
-                    this.id = id;
-                    this.sensors = new ArrayList<>();
-                }
-
-                public String getId() {
-                    return id;
-                }
-
-                public List<Sensor> getSensors() {
-                    return sensors;
-                }
+            @Override
+            public void onBinaryReceived(byte[] data) {
+                Log.i("WebSocket", "Odebrano dane binarne");
             }
 
             @Override
-            public void onBinaryReceived(byte[] data) {}
+            public void onPingReceived(byte[] data) {
+                Log.i("WebSocket", "Odebrano ping");
+            }
 
             @Override
-            public void onPingReceived(byte[] data) {}
-
-            @Override
-            public void onPongReceived(byte[] data) {}
+            public void onPongReceived(byte[] data) {
+                Log.i("WebSocket", "Odebrano pong");
+            }
 
             @Override
             public void onException(Exception e) {
-                e.printStackTrace();
+                Log.e("WebSocket", "Wystąpił wyjątek: " + e.getMessage());
             }
 
             @Override
             public void onCloseReceived() {
-                Log.i("WebSocket", "Closed ");
+                Log.i("WebSocket", "Zakończono sesję");
             }
         };
 
@@ -207,4 +291,7 @@ public class ChartActivity extends AppCompatActivity {
         webSocketClient.enableAutomaticReconnection(5000);
         webSocketClient.connect();
     }
+
+
+
 }
